872e3aa380dbde8324fd3f0f623f7064195a57b8
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-03-27 22:23:25 -0500

    added inf long pole and inf long blade

--- src/main/java/com/existingeevee/srp_spartan_weaponry/init/ParasiteSWItems.java
+++ src/main/java/com/existingeevee/srp_spartan_weaponry/init/ParasiteSWItems.java
@@ -1,7 +1,31 @@
 package com.existingeevee.srp_spartan_weaponry.init;
 
+import com.dhanantry.scapeandrunparasites.SRPMain;
+import com.existingeevee.srp_spartan_weaponry.SRPSpartanWeaponry;
+import com.oblivioussp.spartanweaponry.init.ModelRenderRegistry;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
 public class ParasiteSWItems {
 
+	public static Item infLongBlade = new Item().setCreativeTab(SRPMain.SRP_CREATIVETAB).setTranslationKey("infectious_long_blade_fragment").setRegistryName("infectious_long_blade_fragment");
+	public static Item hardbonePole = new Item().setCreativeTab(SRPMain.SRP_CREATIVETAB).setTranslationKey("hardened_bone_pole").setRegistryName("hardened_bone_pole");
+	
+	@SubscribeEvent
+	public static void onItemRegisterEvent(RegistryEvent.Register<Item> ev) {
+		ev.getRegistry().register(infLongBlade);
+		ModelRenderRegistry.addItemToRegistry(infLongBlade, 
+				new ResourceLocation(SRPSpartanWeaponry.MOD_ID, "components/" + infLongBlade.getRegistryName().getPath()));
 
+		ev.getRegistry().register(hardbonePole);
+		ModelRenderRegistry.addItemToRegistry(hardbonePole, 
+				new ResourceLocation(SRPSpartanWeaponry.MOD_ID, "components/" + hardbonePole.getRegistryName().getPath()));
+
+	} 
 	
 }
