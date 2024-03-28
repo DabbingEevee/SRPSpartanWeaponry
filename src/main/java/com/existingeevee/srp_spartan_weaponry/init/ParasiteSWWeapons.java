4738e576f796dc74d28ddd48d2d0f052519e0571
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-03-28 00:21:46 -0500

    added basically everything except rapier, parrying knife, mace, and quarterstaff

--- src/main/java/com/existingeevee/srp_spartan_weaponry/init/ParasiteSWWeapons.java
+++ src/main/java/com/existingeevee/srp_spartan_weaponry/init/ParasiteSWWeapons.java
@@ -4,12 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.existingeevee.srp_spartan_weaponry.SRPSpartanWeaponry;
+import com.existingeevee.srp_spartan_weaponry.Utils;
 import com.oblivioussp.spartanweaponry.api.IWeaponPropertyContainer;
 import com.oblivioussp.spartanweaponry.api.SpartanWeaponryAPI;
 import com.oblivioussp.spartanweaponry.api.ToolMaterialEx;
 import com.oblivioussp.spartanweaponry.client.gui.CreativeTabsSW;
 import com.oblivioussp.spartanweaponry.init.ModelRenderRegistry;
 import com.oblivioussp.spartanweaponry.item.ItemDagger;
+import com.oblivioussp.spartanweaponry.item.ItemGlaive;
+import com.oblivioussp.spartanweaponry.item.ItemGreatsword;
+import com.oblivioussp.spartanweaponry.item.ItemHalberd;
+import com.oblivioussp.spartanweaponry.item.ItemHammer;
+import com.oblivioussp.spartanweaponry.item.ItemJavelin;
+import com.oblivioussp.spartanweaponry.item.ItemKatana;
+import com.oblivioussp.spartanweaponry.item.ItemLongsword;
+import com.oblivioussp.spartanweaponry.item.ItemPike;
+import com.oblivioussp.spartanweaponry.item.ItemSaber;
+import com.oblivioussp.spartanweaponry.item.ItemSpear;
+import com.oblivioussp.spartanweaponry.item.ItemThrowingAxe;
+import com.oblivioussp.spartanweaponry.item.ItemThrowingKnife;
+import com.oblivioussp.spartanweaponry.item.ItemWarhammer;
 
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
@@ -21,16 +35,70 @@ import net.minecraftforge.registries.IForgeRegistry;
 @Mod.EventBusSubscriber
 public class ParasiteSWWeapons {
 
-	public static final ToolMaterialEx MATERIAL_LIVING = new ToolMaterialEx("living", "coreLiving", SRPSpartanWeaponry.MOD_ID, -1, -1, 4, 2048, 7.5f, 8.0f, 18);
+	public static final ToolMaterialEx MATERIAL_LIVING = new ToolMaterialEx("living", "$nothing", SRPSpartanWeaponry.MOD_ID, -1, -1, 4, 1000, 7.5f, 5.0f, 18);
 
+	public static ItemGreatsword claymoreLiving = null; 
 	public static ItemDagger daggerLiving = null;
+	public static ItemGlaive glaiveLiving = null;
+	public static ItemHalberd halbardLiving = null;
+	public static ItemHammer hammerLiving = null;
+	public static ItemJavelin javelinLiving = null;
+	public static ItemKatana katanaLiving = null;
+	public static ItemLongsword longswordLiving = null;
+	public static ItemPike pikeLiving = null;
+	public static ItemSaber saberLiving = null;
+	public static ItemSpear spearLiving = null;
+	public static ItemThrowingAxe throwingAxeLiving = null;
+	public static ItemThrowingKnife throwingKnifeLiving = null;
+	public static ItemWarhammer warhammerLiving = null;
 
 	private static List<Item> initalizeItems(List<Item> items) {
-
+		
+		claymoreLiving = (ItemGreatsword) SpartanWeaponryAPI.createGreatsword(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD)
+				.setTranslationKey("claymore_living");
+		Utils.forceSetRegistryName(claymoreLiving, "claymore_living");
+		Utils.resetAutogenName(claymoreLiving);
+		items.add(claymoreLiving);
+		
 		daggerLiving = (ItemDagger) SpartanWeaponryAPI.createDagger(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
-		//daggerLiving.addWeaponProperty(null);
 		items.add(daggerLiving);
 
+		glaiveLiving = (ItemGlaive) SpartanWeaponryAPI.createGlaive(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(glaiveLiving);
+		
+		halbardLiving = (ItemHalberd) SpartanWeaponryAPI.createHalberd(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(halbardLiving);
+		
+		hammerLiving = (ItemHammer) SpartanWeaponryAPI.createHammer(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(hammerLiving);
+		
+		javelinLiving = (ItemJavelin) SpartanWeaponryAPI.createJavelin(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(javelinLiving);
+		
+		katanaLiving = (ItemKatana) SpartanWeaponryAPI.createKatana(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(katanaLiving);
+		
+		longswordLiving = (ItemLongsword) SpartanWeaponryAPI.createLongsword(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(longswordLiving);
+		
+		pikeLiving = (ItemPike) SpartanWeaponryAPI.createPike(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(pikeLiving);
+		
+		saberLiving = (ItemSaber) SpartanWeaponryAPI.createSaber(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(saberLiving);
+		
+		spearLiving = (ItemSpear) SpartanWeaponryAPI.createSpear(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(spearLiving);
+		
+		throwingAxeLiving = (ItemThrowingAxe) SpartanWeaponryAPI.createThrowingAxe(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(throwingAxeLiving);
+		
+		throwingKnifeLiving = (ItemThrowingKnife) SpartanWeaponryAPI.createThrowingKnife(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(throwingKnifeLiving);
+		
+		warhammerLiving = (ItemWarhammer) SpartanWeaponryAPI.createWarhammer(MATERIAL_LIVING, SRPSpartanWeaponry.MOD_ID, CreativeTabsSW.TAB_SW_MOD);
+		items.add(warhammerLiving);
+		
 		return items;
 	}
 
