0ab77a580711ca1d5847288d3bfbf287532a73fb
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-05-01 21:44:29 -0500

    update properties list

--- src/main/java/com/existingeevee/srp_spartan_weaponry/init/ParasiteSWProperties.java
+++ src/main/java/com/existingeevee/srp_spartan_weaponry/init/ParasiteSWProperties.java
@@ -1,8 +1,11 @@
 package com.existingeevee.srp_spartan_weaponry.init;
 
 import com.existingeevee.srp_spartan_weaponry.properties.BleedingWeaponProperty;
+import com.existingeevee.srp_spartan_weaponry.properties.CloakingWeaponProperty;
 import com.existingeevee.srp_spartan_weaponry.properties.CorrosionWeaponProperty;
 import com.existingeevee.srp_spartan_weaponry.properties.ImmalleableWeaponProperty;
+import com.existingeevee.srp_spartan_weaponry.properties.RepulseWeaponProperty;
+import com.existingeevee.srp_spartan_weaponry.properties.ViralWeaponProperty;
 
 public class ParasiteSWProperties {
 	
@@ -13,5 +16,13 @@ public class ParasiteSWProperties {
 	public static final CorrosionWeaponProperty CORROSION_2 = new CorrosionWeaponProperty(2);
 	
 	public static final ImmalleableWeaponProperty IMMALLEABLE = new ImmalleableWeaponProperty();
+	
+	public static final ViralWeaponProperty VIRAL_1 = new ViralWeaponProperty(1);
+	public static final ViralWeaponProperty VIRAL_2 = new ViralWeaponProperty(2);
+	
+	public static final CloakingWeaponProperty CLOAKING = new CloakingWeaponProperty();
+	
+	public static final RepulseWeaponProperty REPULSE = new RepulseWeaponProperty();
+
 	
 }
