f2ab862b5c9f9a8a7911b0f00ae00ff2749b3b77
Sweebow <evan.jeebow@gmail.com>
2024-06-16 13:51:09 -0600

    updated abilities

--- src/main/java/com/existingeevee/swparasites/init/ParasiteSWProperties.java
+++ src/main/java/com/existingeevee/swparasites/init/ParasiteSWProperties.java
@@ -9,23 +9,30 @@ import com.existingeevee.swparasites.properties.HeavyWeaponProperty;
 import com.existingeevee.swparasites.properties.ViralWeaponProperty;
 
 public class ParasiteSWProperties {
-	
+
 	public static final BleedingWeaponProperty BLEEDING_1 = new BleedingWeaponProperty(1);
 	public static final BleedingWeaponProperty BLEEDING_2 = new BleedingWeaponProperty(2);
+	public static final BleedingWeaponProperty BLEEDING_3 = new BleedingWeaponProperty(3);
 
 	public static final CorrosionWeaponProperty CORROSION_1 = new CorrosionWeaponProperty(1);
 	public static final CorrosionWeaponProperty CORROSION_2 = new CorrosionWeaponProperty(2);
-	
-	public static final ImmalleableWeaponProperty IMMALLEABLE = new ImmalleableWeaponProperty();
-	
+	public static final CorrosionWeaponProperty CORROSION_3 = new CorrosionWeaponProperty(3);
+
+	public static final ImmalleableWeaponProperty IMMALLEABLE_1 = new ImmalleableWeaponProperty(1);
+	public static final ImmalleableWeaponProperty IMMALLEABLE_2 = new ImmalleableWeaponProperty(2);
+
 	public static final ViralWeaponProperty VIRAL_1 = new ViralWeaponProperty(1);
 	public static final ViralWeaponProperty VIRAL_2 = new ViralWeaponProperty(2);
-	
-	public static final CloakingWeaponProperty CLOAKING = new CloakingWeaponProperty();
-	
-	public static final RepulseWeaponProperty REPULSE = new RepulseWeaponProperty();
-	
-	public static final HeavyWeaponProperty SLOW = new HeavyWeaponProperty();
+	public static final ViralWeaponProperty VIRAL_3 = new ViralWeaponProperty(3);
 
-	
+	public static final CloakingWeaponProperty CLOAKING_1 = new CloakingWeaponProperty(1);
+	public static final CloakingWeaponProperty CLOAKING_2 = new CloakingWeaponProperty(2);
+
+	public static final RepulseWeaponProperty REPULSE_1 = new RepulseWeaponProperty(1);
+	public static final RepulseWeaponProperty REPULSE_2 = new RepulseWeaponProperty(2);
+
+	public static final HeavyWeaponProperty SLOW_1 = new HeavyWeaponProperty(1);
+	public static final HeavyWeaponProperty SLOW_2 = new HeavyWeaponProperty(2);
+
+
 }
