bd76e8cc95e2493730947b407755c38769fc9f0e
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-08-19 13:24:20 -0500

    impaler sorta works

--- src/main/java/com/existingeevee/swparasites/Utils.java
+++ src/main/java/com/existingeevee/swparasites/Utils.java
@@ -1,7 +1,19 @@
 package com.existingeevee.swparasites;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 import com.oblivioussp.spartanweaponry.item.ItemSwordBase;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -48,5 +60,58 @@ public class Utils {
 				executor.run();
 			}
 		}.start(executeIn);
+	}
+	
+	public static RayTraceResult rayTrace(EntityLivingBase entityLiving, double maxRange, List<Entity> exclude) {
+		return rayTrace(entityLiving, maxRange, exclude, true);
+	}
+
+
+	public static RayTraceResult rayTrace(EntityLivingBase entityLiving, double maxRange, List<Entity> exclude, boolean affectedByBlocks) {
+		Vec3d start = entityLiving.getPositionEyes(0.5f);
+		Vec3d lookVec = entityLiving.getLookVec();
+
+		exclude = exclude == null ? new ArrayList<>() : new ArrayList<>(exclude);
+		exclude.add(entityLiving);
+
+		return rayTrace(start, lookVec, entityLiving.world, maxRange, exclude::contains, affectedByBlocks, false);
+	}
+
+	public static RayTraceResult rayTrace(Vec3d start, Vec3d direction, World world, double maxRange, Predicate<Entity> exclude, boolean affectedByBlocks, boolean ignoreNoBounding) {
+		Vec3d end = start.add(direction.scale(maxRange));
+		RayTraceResult firstTrace = affectedByBlocks ? world.rayTraceBlocks(start, end, false, ignoreNoBounding, true) : null;
+		AxisAlignedBB area = new AxisAlignedBB(start, firstTrace != null ? firstTrace.hitVec : end);
+		List<Entity> entities = world.getEntitiesWithinAABBExcludingEntity(null, area);
+
+		Entity closestValid = null;
+		double closestDistSq = Double.MAX_VALUE;
+
+		for (Entity e : entities) {
+			if (!(e instanceof EntityLivingBase) || (exclude != null && exclude.test(e))) {
+				continue;
+			}
+
+			RayTraceResult intercept = e.getEntityBoundingBox().calculateIntercept(start, end);
+
+			if (intercept != null) {
+				double distSq = intercept.hitVec.squareDistanceTo(start);
+				if (closestDistSq > distSq) {
+					closestValid = e;
+					closestDistSq = distSq;
+				}
+			}
+		}
+
+		if (closestValid != null) {
+			return new RayTraceResult(closestValid);
+		} else if (firstTrace != null) {
+			return firstTrace;
+		} else {
+			return new RayTraceResult(RayTraceResult.Type.MISS, end, EnumFacing.DOWN, new BlockPos(end));
+		}
+	}
+	
+	public static Vec3d getCenter(AxisAlignedBB box) {
+		return new Vec3d(box.minX + (box.maxX - box.minX) * 0.5D, box.minY + (box.maxY - box.minY) * 0.5D, box.minZ + (box.maxZ - box.minZ) * 0.5D);
 	}
 }
--- src/main/java/com/existingeevee/swparasites/items/ItemImpalerShield.java
+++ src/main/java/com/existingeevee/swparasites/items/ItemImpalerShield.java
@@ -1,8 +1,10 @@
 package com.existingeevee.swparasites.items;
 
+import com.existingeevee.swparasites.Utils;
 import com.oblivioussp.spartanshields.item.ItemShieldBase;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -10,20 +12,22 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ItemImpalerShield extends ItemShieldBase {
 
-	int level = 0;
+	float attackDamage = 15;
+	float falloffPerBlock = 3; // radians
+	double maxRange = 3;
+	double maxDeltaAngle = Math.PI / 4; // radians
 
-	public ItemImpalerShield(String unlocName, int maxDurability, int damageLevel) {
+	public ItemImpalerShield(String unlocName, int maxDurability, float damageLevel) {
 		super(unlocName);
 
-		this.level = damageLevel;
+		this.attackDamage = damageLevel;
 		this.setMaxDamage(maxDurability);
 
 		MinecraftForge.EVENT_BUS.register(this);
@@ -32,32 +36,64 @@ public class ItemImpalerShield extends ItemShieldBase {
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		ItemStack item = playerIn.getHeldItem(handIn);
-		if (!playerIn.isSneaking() || playerIn.getCooldownTracker().getCooldown(item.getItem(), 0) > 0) { 
-			return null;
+		if (!playerIn.isSneaking()) {
+			return super.onItemRightClick(worldIn, playerIn, handIn);
 		}
-		
+
 		Vec3d vec = playerIn.getLookVec();
-		
-        playerIn.motionX += vec.x * 1.5;
-        playerIn.motionY += 1;
-        playerIn.motionZ += vec.z * 1.5;
-        playerIn.velocityChanged = true;
-        
-		AxisAlignedBB box = new AxisAlignedBB(playerIn.posX, playerIn.posY, playerIn.posZ, playerIn.posX, playerIn.posY, playerIn.posZ).grow(1);
-        
-		for (Entity entity : worldIn.getEntitiesWithinAABBExcludingEntity(playerIn, box))
-		{
-			Vec3d motionVector = new Vec3d(entity.posX - playerIn.posX, 0.6D, entity.posZ - playerIn.posZ).normalize();
-			entity.attackEntityFrom(DamageSource.causePlayerDamage(playerIn), 15 * level);
-			if (!worldIn.isRemote)
-			{
-				entity.motionX += motionVector.x * 1.5D;
+
+		playerIn.motionX += vec.x * 1.5;
+		playerIn.motionY += vec.y * 1.5;
+		playerIn.motionZ += vec.z * 1.5;
+		playerIn.velocityChanged = true;
+
+		AxisAlignedBB box = new AxisAlignedBB(playerIn.posX, playerIn.posY, playerIn.posZ, playerIn.posX, playerIn.posY, playerIn.posZ).grow(maxRange);
+
+		for (Entity entity : worldIn.getEntitiesInAABBexcluding(playerIn, box, e -> isValidTarget(e, playerIn))) {
+			
+			//Deal the proper damage
+			Vec3d eyePos = playerIn.getPositionEyes(0.5f);
+			Vec3d targetCenterPos = Utils.getCenter(entity.getEntityBoundingBox());
+			float distanceTo = (float) eyePos.distanceTo(targetCenterPos);
+			entity.attackEntityFrom(DamageSource.causePlayerDamage(playerIn), attackDamage - distanceTo * falloffPerBlock);
+			
+			Vec3d motionVector = new Vec3d(entity.posX - playerIn.posX, 0, entity.posZ - playerIn.posZ).normalize()
+					.scale(1.5) //1.5 m/s of initial push
+					.scale(1 - distanceTo / (maxRange * 2)) //put some falloff on it
+					.add(0, 0.6D, 0); //bit of an upwards push as well bc why not
+
+			
+			if (!worldIn.isRemote) {
+				entity.motionX += motionVector.x;
 				entity.motionY += motionVector.y;
-				entity.motionZ += motionVector.z * 1.5D;
+				entity.motionZ += motionVector.z;
 				entity.velocityChanged = true;
 			}
 		}
-        playerIn.getCooldownTracker().setCooldown(item.getItem(), 50);
-	    return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
+		playerIn.getCooldownTracker().setCooldown(item.getItem(), 50);
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
+	}
+
+	private boolean isValidTarget(Entity target, EntityPlayer player) {
+		if (target == null || player == null)
+			return false;
+
+		Vec3d eyePos = player.getPositionEyes(0.5f);
+		Vec3d targetCenterPos = Utils.getCenter(target.getEntityBoundingBox());
+
+		Vec3d lookVec = player.getLookVec();
+		Vec3d targetDirection = targetCenterPos.subtract(eyePos).normalize();
+
+		double deltaAngle = Math.abs(Math.acos(lookVec.dotProduct(targetDirection)));
+
+		if (deltaAngle > maxDeltaAngle) //Too far off the angle. nope.
+			return false;
+
+		RayTraceResult result = Utils.rayTrace(eyePos, targetDirection, player.world, this.maxRange, e -> e != target, true, true);
+	
+		if (result.entityHit != target) //Too far away or obstructed by blocks. nope
+			return false;
+		
+		return target instanceof EntityLivingBase; //they have to be living smhhh
 	}
 }
