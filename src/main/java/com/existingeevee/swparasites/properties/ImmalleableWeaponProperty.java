fb3e163997d14a052e03cd6589f42a8ebf43e218
Sweebow <evan.jeebow@gmail.com>
2024-09-03 23:32:00 -0600

    ev help it broked :(

--- src/main/java/com/existingeevee/swparasites/properties/ImmalleableWeaponProperty.java
+++ src/main/java/com/existingeevee/swparasites/properties/ImmalleableWeaponProperty.java
@@ -1,15 +1,18 @@
 package com.existingeevee.swparasites.properties;
 
 import com.dhanantry.scapeandrunparasites.init.SRPPotions;
+import com.dhanantry.scapeandrunparasites.init.SRPSounds;
 import com.existingeevee.swparasites.SRPSpartanWeaponry;
 import com.oblivioussp.spartanweaponry.api.ToolMaterialEx;
 import com.oblivioussp.spartanweaponry.api.weaponproperty.WeaponPropertyWithCallback;
+import com.oblivioussp.spartanweaponry.item.ItemThrowingWeapon;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.SoundCategory;
 
 public class ImmalleableWeaponProperty extends WeaponPropertyWithCallback {
 
@@ -19,12 +22,23 @@ public class ImmalleableWeaponProperty extends WeaponPropertyWithCallback {
 
 	@Override
 	public void onHitEntity(ToolMaterialEx material, ItemStack stack, EntityLivingBase target, EntityLivingBase attacker, Entity projectile) {
-		double chance = this.getMagnitude() / 8;
-		if (attacker instanceof EntityPlayer && ((EntityPlayer) attacker).getCooledAttackStrength(1f) <= 0.9) {
-			return;
-		}
-		if (attacker.world.rand.nextDouble() < chance) {
-			target.addPotionEffect(new PotionEffect(SRPPotions.RES_E, 40, 0, false, false));
+		if (!attacker.world.isRemote) {
+			double chance = this.getMagnitude() / 8;
+			ItemStack tool = ((EntityPlayer) attacker).getHeldItemMainhand();
+			int cooldown = (int) ((EntityPlayer) attacker).getCooldownTracker().getCooldown(tool.getItem(), 0);
+			
+			System.out.println("cooldown: " + cooldown); 
+			
+			if (attacker instanceof EntityPlayer && cooldown > 0) {
+				return;
+			}
+			if (attacker.world.rand.nextDouble() < chance) {
+				target.addPotionEffect(new PotionEffect(SRPPotions.RES_E, 40, 0, false, false));
+				if (!(stack.getItem() instanceof ItemThrowingWeapon)) {
+					((EntityPlayer) attacker).getCooldownTracker().setCooldown(stack.getItem(), 60);
+				}
+				attacker.world.playSound(null, attacker.posX, attacker.posY, attacker.posZ, SRPSounds.ADAPTATION_P, SoundCategory.PLAYERS, 0.7F, 0.5F);
+			}
 		}
 	}
 }
