8c2907f1c4b645d9616738b83dc97c2c0351bdc8
Sweebow <evan.jeebow@gmail.com>
2024-12-11 17:08:41 -0700

    one must imagine sweebow happy
    https://tenor.com/view/cat-sisyphus-slide-espresso-gif-16709923176714960132

--- src/main/java/com/existingeevee/swparasites/items/ItemParasiteGauntlet.java
+++ src/main/java/com/existingeevee/swparasites/items/ItemParasiteGauntlet.java
@@ -8,6 +8,7 @@ import com.oblivioussp.spartanweaponry.item.ItemCaestus;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -20,7 +21,7 @@ import net.minecraftforge.common.MinecraftForge;
 
 public class ItemParasiteGauntlet extends ItemCaestus implements IHasSRPEvolutionProgress {
 	
-	private boolean toggle = false;
+//	private boolean toggle = false;
 	
 	public ItemParasiteGauntlet(String unlocName, ToolMaterialEx material) {
 		super(unlocName, material);
@@ -29,64 +30,70 @@ public class ItemParasiteGauntlet extends ItemCaestus implements IHasSRPEvolutio
 
 		MinecraftForge.EVENT_BUS.register(this);
 	}
-
+	
 	@Override
-	public boolean onEntitySwing(EntityLivingBase attacker, ItemStack stack) {
-		System.out.println("let me also test if this works: " + Utils.getOrCreateTag(stack).getBoolean("PunchOffhand"));
-		
+    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
 		if (!usingBothGauntlets((EntityPlayer) attacker)) {
 			System.out.println("gauntlet not detected, getting the hell out");
 			return true;
 		}
 		
 		System.out.println("gauntlet detected, continuing");
+
+		EntityPlayer player = (EntityPlayer) attacker;
 		
-		boolean punchOffhand = Utils.getOrCreateTag(stack).getBoolean("PunchOffhand");
-		
-		System.out.println("offhand punch?" + punchOffhand);
-		
-		if (punchOffhand) {
-			System.out.println("offhand punch should have happened here");
-//			attacker.swingArm(EnumHand.OFF_HAND); disabled until ev finds solution
-		}
-		
-		else {
-			System.out.println("mainhand punch should have happened here");
-//			attacker.swingArm(EnumHand.MAIN_HAND); disabled until ev finds solution
-		}
-		
-		Utils.getOrCreateTag(stack).setBoolean("PunchOffhand", !punchOffhand);
-		
-		System.out.println("also here too: " + Utils.getOrCreateTag(stack).getBoolean("PunchOffhand"));
+		player.attackTargetEntityWithCurrentItem(target);
 		
     	return true;
 	}
 	
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-		ItemStack item = playerIn.getHeldItem(handIn);
-		if (!playerIn.onGround) {
-			return super.onItemRightClick(worldIn, playerIn, handIn);
+	public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
+		if (usingBothGauntlets(playerIn) && !target.world.isRemote) {
+			ItemStack itemMain = playerIn.getHeldItem(EnumHand.MAIN_HAND);
+			ItemStack itemOff = playerIn.getHeldItem(EnumHand.OFF_HAND);
+			
+			playerIn.swingArm(EnumHand.OFF_HAND);
+			playerIn.setHeldItem(EnumHand.MAIN_HAND, itemOff);
+			
+			playerIn.attackTargetEntityWithCurrentItem(target);
+			
+			playerIn.setHeldItem(EnumHand.MAIN_HAND, itemMain);
+			
+			// if entity killed from this attack, then add kill count to both gauntlets, each gauntlet gets max hp of target / 2 points
+			
+			
 		}
 		
-		WeaponProperty shockwave = this.getFirstWeaponPropertyWithType("shockwave");
-		
-
-		boolean lvl2 = shockwave.getLevel() != 1;
-		if (shockwave != null) {
-			if (!worldIn.isRemote) {
-			playerIn.world.playSound(null, playerIn.posX, playerIn.posY, playerIn.posZ,
-					SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.PLAYERS, 0.7F, lvl2 ? 0.5f : 1.5f);
-			}
-		}
-		
-		
-		playerIn.getCooldownTracker().setCooldown(item.getItem(), playerIn.isPotionActive(SRPPotions.RAGE_E) ? 50 : 100);
-		
-		playerIn.swingArm(handIn);
-		
-		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
-	}
+        return true;
+    }
+	
+//	@Override
+//	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+//		ItemStack item = playerIn.getHeldItem(handIn);
+//
+//		if (playerIn.onGround && playerIn.isSneaking()) {
+//			WeaponProperty shockwave = this.getFirstWeaponPropertyWithType("shockwave");
+//			
+//
+//			boolean lvl2 = shockwave.getLevel() != 1;
+//			if (shockwave != null) {
+//				if (!worldIn.isRemote) {
+//				playerIn.world.playSound(null, playerIn.posX, playerIn.posY, playerIn.posZ,
+//						SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.PLAYERS, 0.7F, lvl2 ? 0.5f : 1.5f);
+//				}
+//			}
+//			
+//			
+//			playerIn.getCooldownTracker().setCooldown(item.getItem(), playerIn.isPotionActive(SRPPotions.RAGE_E) ? 50 : 100);
+//			
+//			playerIn.swingArm(handIn);
+//			
+//			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
+//		}
+//		
+//		return super.onItemRightClick(worldIn, playerIn, handIn);
+//	}
 	
 	
 	@Override
@@ -96,11 +103,10 @@ public class ItemParasiteGauntlet extends ItemCaestus implements IHasSRPEvolutio
     }
 	
 	public boolean usingBothGauntlets(EntityPlayer player) {
-		Item itemOff = player.getHeldItem(EnumHand.OFF_HAND).getItem();
-		
-		if (itemOff == this) {
+		if (player.getHeldItem(EnumHand.OFF_HAND).getItem() == player.getHeldItem(EnumHand.MAIN_HAND).getItem()) {
 			return true;
 		}
 		return false;
 	}
+
 }
